faeacb2b4a4d5e740ed1f8bc29e53c5c4b37752e
roghayehCoding <r.yalmeh.93@gmail.com>
2023-05-26 23:30:13 +0400

    js applied

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-
-
-function showCity (response){
-    let cityInput = document.querySelector("#search-form");
-    let  h1 = document.querySelector("#city");
-
+function displayTempreature(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement =document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement= document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML =Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humiidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
-
-let apikey = "2f956f5ab16ab6efbf104146e5940f9d";
-let apiURl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
- axios.get(apiURl).then (showCity);
+let apikey = "d154fc7005a41959d58d6c5664de965a";
+let apiURl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apikey}&units=metric`;
+axios.get(apiURl).then(displayTempreature);
