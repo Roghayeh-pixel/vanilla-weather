62c2717c670baa0f2491392faa9a1af29212a166
roghayehCoding <r.yalmeh.93@gmail.com>
2023-05-29 09:33:30 +0400

    added search form

--- index.js
+++ index.js
@@ -28,15 +28,32 @@ function displayTempreature(response){
     let humidityElement= document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
-    //temperatureElement.innerHTML =Math.round(response.data.main.temp);
+    let iconElement = document.querySelector("#icon");
+    temperatureElement.innerHTML =Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humiidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate (response.data.dt * 1000);
+    iconElement.setAttribute("alt", response.data.condition.description);
 }
  
-let city = "New York";
-let apikey = "d154fc7005a41959d58d6c5664de965a"
-let apiURl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
-axios.get(apiURl).then(displayTempreature);
+function search(city){
+let apiKey = "d154fc7005a 41959d58d6c5664de965a";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get(apiUrl).then(displayTempreature);
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Sydney");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+//let city = "New York";
+//let apikey = "d154fc7005a41959d58d6c5664de965a"
+//let apiURl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+//axios.get(apiURl).then(displayTempreature);
